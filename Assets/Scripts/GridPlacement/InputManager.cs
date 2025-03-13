48b077fc60b9beafd5d2bbd17ff5efb70a596f50
SoltidueMarquez <2630388173@qq.com>
2025-03-13 19:49:11 +0800

    初始化-鼠标与网格指示器

---
+++ Assets/Scripts/GridPlacement/InputManager.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GridPlacement
+{
+    public class InputManager : Singleton<InputManager>
+    {
+        [SerializeField, Tooltip("场景相机")] private Camera sceneCamera;
+
+        private Vector3 lastPosition;
+
+        [SerializeField, Tooltip("放置层")] private LayerMask placementLayerMask;
+
+        [SerializeField, Tooltip("最大射线检测距离")] private float maxRayDistance = 100f;
+
+        [SerializeField, Tooltip("放置按键"), Range(0, 1)] private int placeKey = 0;
+        [SerializeField, Tooltip("取消按键")] private KeyCode cancelKey = KeyCode.Escape;
+
+        public event Action OnClicked, OnExit;
+
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(placeKey))
+                OnClicked?.Invoke();
+            if(Input.GetKeyDown(cancelKey))
+                OnExit?.Invoke();
+        }
+
+        public bool IsPointerOverUI() 
+            => EventSystem.current.IsPointerOverGameObject();
+        
+        public Vector3 GetSelectedMapPosition()
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = sceneCamera.nearClipPlane;
+            var ray = sceneCamera.ScreenPointToRay(mousePos);// 从鼠标位置打出一条射线
+            if (Physics.Raycast(ray, out var hit, maxRayDistance, placementLayerMask))
+            {
+                lastPosition = hit.point;// 获取射线打到的点
+            }
+
+            return lastPosition;
+        }
+    }
+}
+
---
+++ Assets/Scripts/GridPlacement/PlacementSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace GridPlacement
+{
+    public class PlacementSystem : MonoBehaviour
+    {
+        [SerializeField,Tooltip("鼠标指示器")] private GameObject mouseIndicator;
+        [SerializeField,Tooltip("单元格指示器")] private GameObject cellIndicator;
+        [SerializeField,Tooltip("网格")] private Grid grid;
+        private InputManager inputManager;
+
+        private void Start()
+        {
+            inputManager = InputManager.Instance;
+        }
+
+        private void Update()
+        {
+            var mousePosition = inputManager.GetSelectedMapPosition();// 获取鼠标位置
+            var gridPosition = grid.WorldToCell(mousePosition);// 计算单元格指示器的位置
+            mouseIndicator.transform.position = mousePosition;
+            cellIndicator.transform.position = gridPosition;
+        }
+    }
+}
